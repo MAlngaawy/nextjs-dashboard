829b744a1fdf9650bc121d249ae1eb4faa44b533
Mohamad AbdelMon’em <mabdelmonem.c@saip.gov.sa>
2023-12-18 18:25:58 +0200

    handle fonts and inages

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter, Space_Mono, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const spaceMono = Space_Mono({ weight: '400', subsets: ['latin'] });
+export const lusitana = Lusitana({ weight: '400', subsets: ['latin'] });
