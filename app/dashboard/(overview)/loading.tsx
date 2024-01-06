dfe72c39a638b6bcb4e9a7038cc80506a0f7cdcb
Mohamad AbdelMon’em <mabdelmonem.c@saip.gov.sa>
2024-01-06 18:06:50 +0200

    stream charts and latest revinue

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import DashboardSkeleton from '../../ui/skeletons';
+
+type Props = {};
+
+function Loading({}: Props) {
+  return <DashboardSkeleton />;
+}
+
+export default Loading;
